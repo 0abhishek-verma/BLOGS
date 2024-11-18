d8437f0c8769dcb488400ef12f4d0d6c29586730
Abhishek250509 <av2000546@gmail.com>
2024-11-19 02:35:27 +0530

    database

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -65,7 +65,60 @@ export class Service{
                 
             }
         }
-        
+        async getPost(slug){
+            try {
+                return await this.databases.getDocument(
+                    conf.appwriteDatabaseId,
+                    conf.appwriteCollectionId,
+                    slug
+                )
+            } catch (error) {
+                throw error;
+            }
+        }
+        async getPosts(queries=[Query.equal("status","active")]){
+            try {
+                return await this.databases.listDocuments(
+                    conf.appwriteCollectionId,
+                    conf.appwriteDatabaseId,
+                    queries
+                )
+            } catch (error) {
+                throw error;
+            }
+        }
+
+        //FILE UPLOAD SERVICE
+
+        async uploadFile(file){
+            try {
+                return await this.bucket.createFile(
+                    conf.appwriteBucketId,
+                    ID.unique(),
+                    file
+                )
+            } catch (error) {
+                throw error;
+            }
+        }
+
+        async deleteFile(fileId){
+            try {
+                return await this.bucket.deleteFile(
+                    conf.appwriteBucketId,
+                    fileId
+                )
+            } catch (error) {
+                throw error;
+            }
+        }
+
+        getFilePreview(fileId){
+            return this.bucket.getFilePreview(
+                conf.appwriteBucketId,
+                fileId
+            )
+        }
 }
 
 const service = new Service()
